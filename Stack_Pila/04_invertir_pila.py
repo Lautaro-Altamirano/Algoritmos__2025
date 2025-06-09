d1823dafe2782f5d13fb828a29fc3725a5405a86
Lautaro-Altamirano <laualtamirano2@gmail.com>
2025-06-09 03:09:20 -0300

    Update 04_invertir_pila.py

--- Stack_Pila/04_invertir_pila.py
+++ Stack_Pila/04_invertir_pila.py
@@ -5,17 +5,18 @@ from stack import Stack
 
 pila_pricipal = Stack()
 pila_aux = Stack()
-print("Pila original :")
+
 for i in range(5):# cargamos pila (stack)
     numero_random = randint(1,20)
     print(numero_random)
     pila_pricipal.push(numero_random)
+    
+print("Pila original :")
+pila_principal.show()
 
 while pila_pricipal.size() > 0 :
     eliminado = pila_pricipal.pop()
     pila_aux.push(eliminado)
-while pila_aux.size() > 0 :
-    pila_pricipal.push(pila_aux.pop())
     
 print("Pila invertida :")
-pila_pricipal.show()
+pila_aux.show()
